896f8a2cf4da0c897ca74cb88ab7d1d3d9439498
jadelbied <jadelbied@gmail.com>
2024-01-24 01:20:45 +0100

    Delete and update

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { PostCarComponent } from './components/post-car/post-car.component';
 import { NgZorroImportsModule } from '../../NgZorroImportsModule';
+import { UpdateCarComponent } from './components/update-car/update-car.component';
 
 @NgModule({
-  declarations: [AdminDashboardComponent, PostCarComponent],
+  declarations: [AdminDashboardComponent, PostCarComponent, UpdateCarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AdminDashboardComponent {
 
+  cars:any=[];
+
+   constructor(private adminService:AdminService,private message:NzMessageService){}
+
+   ngOnInit(){
+    this.getAllCars();
+   }
+   getAllCars(){
+    this.adminService.getAllCars().subscribe((res)=>{
+      console.log(res);
+      res.forEach((element:any) => {
+        element.processingImg='data:image/jpeg;base64,'+element.returnedImage;
+        this.cars.push(element);
+      });
+    })
+   }
+   deleteCar(id:number){
+      console.log(id);
+      this.adminService.deleteCar(id).subscribe((res)=>{
+        this.getAllCars();
+        this.message.success("Car deleted succesfully",{nzDuration:5000});
+      })
+   }
 }
